af33d7fa116b358fd39452c89a3de9bba116bc52
Reginaldo Lopes <rgllopes@hotmail.com>
2022-12-17 11:38:09 -0300

    Fim seção 7

---
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        public MySQLContext() { }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
